0ae48f5b2dd05b660568902fcde83ab77f6b3269
Nikolai Emil Damm <neq@energinet.dk>
2025-02-26 11:25:35 +0100

    refactor: uncomment Helm binary download commands in get-helm-binary.sh
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- .github/scripts/get-helm-binary.sh
+++ .github/scripts/get-helm-binary.sh
@@ -24,12 +24,12 @@ get() {
 }
 
 latest=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r .tag_name)
-#get "https://get.helm.sh/helm-$latest-darwin-amd64.tar.gz" "darwin-amd64/helm" "src/Devantler.HelmCLI/runtimes/osx-x64/native" "helm-osx-x64" "tar"
-#get "https://get.helm.sh/helm-$latest-darwin-arm64.tar.gz" "darwin-arm64/helm" "src/Devantler.HelmCLI/runtimes/osx-arm64/native" "helm-osx-arm64" "tar"
-#get "https://get.helm.sh/helm-$latest-linux-amd64.tar.gz" "linux-amd64/helm" "src/Devantler.HelmCLI/runtimes/linux-x64/native" "helm-linux-x64" "tar"
-#get "https://get.helm.sh/helm-$latest-linux-arm64.tar.gz" "linux-arm64/helm" "src/Devantler.HelmCLI/runtimes/linux-arm64/native" "helm-linux-arm64" "tar"
-#get "https://get.helm.sh/helm-$latest-windows-amd64.zip" "windows-amd64/helm.exe" "src/Devantler.HelmCLI/runtimes/win-x64/native" "helm-win-x64.exe" "zip"
-#get "https://get.helm.sh/helm-$latest-windows-arm64.zip" "windows-arm64/helm.exe" "src/Devantler.HelmCLI/runtimes/win-arm64/native" "helm-win-arm64.exe" "zip"
+get "https://get.helm.sh/helm-$latest-darwin-amd64.tar.gz" "darwin-amd64/helm" "src/Devantler.HelmCLI/runtimes/osx-x64/native" "helm-osx-x64" "tar"
+get "https://get.helm.sh/helm-$latest-darwin-arm64.tar.gz" "darwin-arm64/helm" "src/Devantler.HelmCLI/runtimes/osx-arm64/native" "helm-osx-arm64" "tar"
+get "https://get.helm.sh/helm-$latest-linux-amd64.tar.gz" "linux-amd64/helm" "src/Devantler.HelmCLI/runtimes/linux-x64/native" "helm-linux-x64" "tar"
+get "https://get.helm.sh/helm-$latest-linux-arm64.tar.gz" "linux-arm64/helm" "src/Devantler.HelmCLI/runtimes/linux-arm64/native" "helm-linux-arm64" "tar"
+get "https://get.helm.sh/helm-$latest-windows-amd64.zip" "windows-amd64/helm.exe" "src/Devantler.HelmCLI/runtimes/win-x64/native" "helm-win-x64.exe" "zip"
+get "https://get.helm.sh/helm-$latest-windows-arm64.zip" "windows-arm64/helm.exe" "src/Devantler.HelmCLI/runtimes/win-arm64/native" "helm-win-arm64.exe" "zip"
 rm -rf src/Devantler.HelmCLI/runtimes/*/native/darwin-*
 rm -rf src/Devantler.HelmCLI/runtimes/*/native/linux-*
 rm -rf src/Devantler.HelmCLI/runtimes/*/native/windows-*
